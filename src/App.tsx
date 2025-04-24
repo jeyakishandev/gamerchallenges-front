e79dd9f1c81abc284e4bab9380c36ad4ef04c34c
kishankarunanithy <jeya-kishan.karunanithy@oclock.school>
2025-04-24 17:07:05 +0200

    probleme with the font not the same

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@ function App() {
     <>
       {/* plus tard le Header ici */}
       <main className="home-content">
-        <h1>Prêt à relever le challenge ?</h1>
+      <h1 className="joti-title">Prêt à relever le challenge ?</h1>
+
         <p>
           Montrez-nous ce que vous avez dans le ventre !<br />
           Postez vos vidéos, défiez les autres, et grimpez au sommet.
         </p>
+        <h1 style={{ fontFamily: 'Joti One, cursive', fontSize: '2rem' }}>
+  Test Font Joti One ?
+</h1>
 
         <div className="home-buttons">
           <button>Créer</button>

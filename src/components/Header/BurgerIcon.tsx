1653a08c8b8f84942a33fe0c063089a60518e73c
SandrineLJ <sandrine.louisjean@gmail.com>
2025-04-24 20:49:31 +0200

    wip: header style

---
+++ src/components/Header/BurgerIcon.tsx
@@ -0,0 +1,37 @@
+export default function BurgerIcon() {
+    return (
+        <>
+            <svg
+                width="100%"
+                height="100%"
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                color="none"
+            >
+                <path
+                d="M3 5H21"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                />
+                <path
+                d="M3 12H21"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                />
+                <path
+                d="M3 19H21"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                />
+            </svg>
+        </>
+    )
+}

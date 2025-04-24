7298ef5b83fcd11856c250cfd7bb685086be7aa7
kishankarunanithy <jeya-kishan.karunanithy@oclock.school>
2025-04-24 15:40:24 +0200

    I do my body to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+
 
 function App() {
   return (
     <>
-     
+      {/* plus tard le Header ici */}
+      <main className="home-content">
+        <h1>Prêt à relever le challenge ?</h1>
+        <p>
+          Montrez-nous ce que vous avez dans le ventre !<br />
+          Postez vos vidéos, défiez les autres, et grimpez au sommet.
+        </p>
+
+        <div className="home-buttons">
+          <button>Créer</button>
+          <button>Participer</button>
+        </div>
+      </main>
+
       <Footer />
     </>
   );
 }
-
 export default App;

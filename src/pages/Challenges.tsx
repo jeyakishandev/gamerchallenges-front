96aa860121ecefd848cb2f80aab8c29f7efb0bf8
SandrineLJ <sandrine.louisjean@gmail.com>
2025-04-26 01:15:25 +0200

    style: improve responsive design

--- src/pages/Challenges.tsx
+++ src/pages/Challenges.tsx
@@ -3,17 +3,15 @@ import ChallengesCards from "../components/ChallengesCards";
 export default function Challenges() {
 
     return (
-        <>
-            <h1>Les Challenges</h1>
+        <main>
+            <h1>Tous les challenges en cours</h1>
             <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Cras auctor, nisi tempus lacinia pulvinar, ligula 
                 est efficitur tortor, sit amet rhoncus justo tellus sed erat.
                 Sed hendrerit mauris id velit</h2>
 
-                <a href="#" className="default-button">Créer un challenge</a>
-
                 <ChallengesCards/>
-        </>
+        </main>
     )
     
 }

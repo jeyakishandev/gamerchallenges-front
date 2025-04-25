fb91709d67dcbc24a3b4990f110fd8e81373d2a6
kishankarunanithy <jeya-kishan.karunanithy@oclock.school>
2025-04-25 15:15:51 +0200

    we do some carrousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,53 @@
-export default function Home () {
-    return (
-<main className="home-content">
-      <h1 className="joti-title">Prêt à relever le challenge ?</h1>
+import "./Home.css";
 
-        <p>
-          Montrez-nous ce que vous avez dans le ventre !<br />
-          Postez vos vidéos, défiez les autres, et grimpez au sommet.
-        </p>
-        <h1 style={{ fontFamily: 'Joti One, cursive', fontSize: '2rem' }}>
-  Test Font Joti One ?
-</h1>
+export default function Home() {
+  return (
+    <main className="home-content">
+      
+      <h1>Prêt à relever le challenge ?</h1>
+      <p>
+        Montrez-nous ce que vous avez dans le ventre ! <br />
+        Postez vos vidéos, défiez les autres, et grimpez au sommet.
+      </p>
 
-        <div className="home-buttons">
-          <button>Créer</button>
-          <button>Participer</button>
+      
+      <div className="home-buttons">
+        <button>Créer</button>
+        <button>Participer</button>
+      </div>
+
+      
+      <section className="carousel-section">
+        <h2>Nouveautés</h2>
+
+        <div className="carousel-container">
+          <button className="arrow left">❮</button>
+
+          <div className="carousel-items">
+            <div className="skeleton-card">jeu 1</div>
+            <div className="skeleton-card">jeu 2</div>
+            <div className="skeleton-card">jeu 3</div>
+            <div className="skeleton-card">jeu 4</div>
+          </div>
+
+          <button className="arrow right">❯</button>
         </div>
-      </main>
-    )
+      </section>
+
+      
+      <section className="carousel-section">
+        <h2>Challenges Populaires</h2>
+        <div className="carousel-container">
+        <button className="arrow left">❮</button>
+
+          <div className="carousel-items">
+            <div className="skeleton-card">populaire 1</div>
+            <div className="skeleton-card">populaire 2</div>
+            <div className="skeleton-card">populaire 3</div>
+          </div>
+          <button className="arrow right">❯</button>
+        </div>
+      </section>
+    </main>
+  );
 }
-
-

950f2d30b956fb80915ea12504df39ceaf5aac56
SandrineLJ <sandrine.louisjean@gmail.com>
2025-04-25 14:10:16 +0200

    feat: add Challenges page and link in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer/Footer";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Challenges from "./pages/Challenges";
 
   
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/challenges/" />
+        <Route path="/challenges/" element={<Challenges />}/>
         <Route path="/challenges/:id" />
         <Route path="/leaderbord/" />
         <Route path="/profile/:id" />
---
+++ src/pages/Challenges.tsx
@@ -0,0 +1,19 @@
+import ChallengesDisplay from "../components/ChallengesDisplay";
+
+export default function Challenges() {
+
+    return (
+        <>
+            <h1>Les Challenges</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Cras auctor, nisi tempus lacinia pulvinar, ligula 
+                est efficitur tortor, sit amet rhoncus justo tellus sed erat.
+                Sed hendrerit mauris id velit</h2>
+
+                <button>Créer un challenge</button>
+
+                <ChallengesDisplay />
+        </>
+    )
+    
+}

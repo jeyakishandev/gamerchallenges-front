1c418912c1252935e143782a40a9636f9e2c37c7
MathiasLeleu <mathiasleleupro@gmail.com>
2025-04-25 17:12:38 +0200

    fix: App.css and Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,34 +3,38 @@ import "./Home.css";
 
 export default function Home () {
     return (
-<main>
-      <h1>Prêt à relever le challenge ?</h1>
+      <>
+        <main>
 
-        <h2>
-          Montrez-nous ce que vous avez dans le ventre !<br />
-          Postez vos vidéos, défiez les autres, et grimpez au sommet.
-        </h2>
-        <div className="buttons-flex">
-          <button>Créer</button>
-          <button>Participer</button>
+          <section>
+            <h1>Prêt à relever le challenge ?</h1>
 
-        </div>
-      </section>
+            <h2>
+              Montrez-nous ce que vous avez dans le ventre !<br />
+              Postez vos vidéos, défiez les autres, et grimpez au sommet.
+            </h2>
+            <div className="buttons-flex">
+              <button>Créer</button>
+              <button>Participer</button>
+
+            </div>
+          </section>
 
       
-      <section className="carousel-section">
-        <h2>Challenges populaire</h2>
-        <div className="carousel-container">
-        <button className="arrow left">❮</button>
+          <section className="carousel-section">
+            <h2>Challenges populaire</h2>
+            <div className="carousel-container">
+              <button className="arrow left">❮</button>
 
-          <div className="carousel-items">
-            <div className="skeleton-card">populaire 1</div>
-            <div className="skeleton-card">populaire 2</div>
-            <div className="skeleton-card">populaire 3</div>
-          </div>
-          <button className="arrow right">❯</button>
-        </div>
-      </section>
-    </main>
+              <div className="carousel-items">
+                <div className="skeleton-card">populaire 1</div>
+                <div className="skeleton-card">populaire 2</div>
+                <div className="skeleton-card">populaire 3</div>
+              </div>
+              <button className="arrow right">❯</button>
+            </div>
+          </section>
+        </main>
+    </>
   );
 }

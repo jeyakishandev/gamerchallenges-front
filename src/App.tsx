8fbad3f3efd19f9ffe3f8dc95175c1da82db779f
redlemon8 <hanquezbenjamin@gmx.com>
2025-04-27 04:30:47 +0200

    feat: get challenge by id

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Footer from "./components/Footer/Footer";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import {Challenge} from "./components/Challenge/Challenge";
 import Challenges from "./pages/Challenges";
 import Leaderboard from "./pages/Leaderboard";
+import { ChallengePage } from "./pages/Challenge";
 
 
   
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/challenges/" element={<Challenges />}/>
-        <Route path="/challenges/:id" element={<Challenge/>}/>
+        <Route path="/challenges/:id" element={<ChallengePage/>}/>
         <Route path="/leaderboard/" element={<Leaderboard />} />
         <Route path="/profile/:id" />
       </Routes>

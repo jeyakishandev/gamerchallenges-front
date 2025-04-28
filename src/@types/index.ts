2b7b152b8094376b67f30ccdd08cb622504e3469
redlemon8 <hanquezbenjamin@gmx.com>
2025-04-28 13:42:50 +0200

    refactor: challenge page

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,3 +1,5 @@
+export type IChallenges = IChallenge[];
+
 export interface IChallenge {
   id: number;
   name: string;
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,10 @@
-import type { IChallenge } from "../@types";
+import type { IChallenges, IChallenge } from "../@types";
+
+export async function getChallenges(): Promise<IChallenges> {
+  const response = await fetch("http://localhost:3000/challenges");
+  const challenges = await response.json();
+  return challenges;
+}
 
 export async function getChallengeById(id: number): Promise<IChallenge> {
   const response = await fetch(

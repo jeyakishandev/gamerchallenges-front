0d6e193b8c8440f1b6aff61d28e6e0bcdf2ae649
MathiasLeleu <mathiasleleupro@gmail.com>
2025-05-05 10:35:42 +0200

    modifying the catchphrase

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -8,7 +8,7 @@ interface ProfilesUsers {
 export default function ProfileInfos({users}: ProfilesUsers) {
     return (
         <div className="perso-info">
-            <h2 className="main-title">Retrouvez ici blabla</h2>
+            <h2 className="main-title">Retrouvez ici ton profile, avec tes informations personnelles, et tes défis !</h2>
             <section className="button-container">
                 <a href="#" className="default-button">Modifier le profile</a>
             </section>

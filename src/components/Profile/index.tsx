f492f2b1732e27dddbd16098a509e5e12494ba4c
MathiasLeleu <mathiasleleupro@gmail.com>
2025-05-05 10:23:41 +0200

    add: creation of the function ProfileUsers, to dynamise the personal information section

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -4,3 +4,16 @@ import { IUser } from "../../@types"
 interface ProfilesUsers {
     users: IUser
 }
+
+export default function ProfileInfos({users}: ProfilesUsers) {
+    return (
+        <div className="perso-info">
+            <h2 className="main-title">Retrouvez ici blabla</h2>
+            <section className="button-container">
+                <a href="#" className="default-button">Modifier le profile</a>
+            </section>
+            <p className="avatar">{users.avatar_url}</p>
+            <p className="pseudo">Pseudo : {users.pseudo}</p>
+        </div>
+    )
+}

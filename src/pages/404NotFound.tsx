19693028e7e88911b75f8bc58a6402b1ac703ed0
MathiasLeleu <mathiasleleupro@gmail.com>
2025-05-07 13:18:03 +0200

    add image to public folder + set up html page structure

--- src/pages/404NotFound.tsx
+++ src/pages/404NotFound.tsx
@@ -2,7 +2,11 @@ export default function NotFound() {
 
     return (
         <>
-        <p>ok</p>
+        <div>
+            <p className="main-sentence-404"></p>
+            <img className="dino-car-404"></img>
+            <p className="home-link-404"></p>
+        </div>
         </>
     )
 }

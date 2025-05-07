bff3015b8ffec53e2553e16fd29724a2dbd175f7
MathiasLeleu <mathiasleleupro@gmail.com>
2025-05-07 13:29:44 +0200

    page 404 (without css) complete

--- src/pages/404NotFound.tsx
+++ src/pages/404NotFound.tsx
@@ -1,11 +1,14 @@
+import "../App.css"
+import { Link } from "react-router-dom"
+
 export default function NotFound() {
 
     return (
         <>
         <div>
-            <p className="main-sentence-404"></p>
-            <img className="dino-car-404"></img>
-            <p className="home-link-404"></p>
+            <p className="main-sentence-404">Ce n'est pas par ici que tu trouveras de nouveaux challenges à réaliser !</p>
+            <img className="dino-car-404" src="../../public/img/dino-4575130_640.png"/>
+            <p className="home-link-404"> <Link to={`/`} > Clique ici </Link> pour retourner à l'acceuil !</p>
         </div>
         </>
     )

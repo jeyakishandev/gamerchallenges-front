1c2267e8faf8ab0b625849e5bbef335a5dc2726d
MathiasLeleu <mathiasleleupro@gmail.com>
2025-05-07 14:10:44 +0200

    page 404 complete

--- src/pages/404NotFound.tsx
+++ src/pages/404NotFound.tsx
@@ -5,10 +5,10 @@ export default function NotFound() {
 
     return (
         <>
-        <div>
-            <p className="main-sentence-404">Ce n'est pas par ici que tu trouveras de nouveaux challenges à réaliser !</p>
+        <div className="main-404">
+            <p className="default-text main-sentence-404">Ce n'est pas par ici que tu trouveras de nouveaux challenges à réaliser !</p>
             <img className="dino-car-404" src="../../public/img/dino-4575130_640.png"/>
-            <p className="home-link-404"> <Link to={`/`} > Clique ici </Link> pour retourner à l'acceuil !</p>
+            <p className="default-text home-link-404"> <Link to={`/`} > Clique ici </Link> pour retourner à l'acceuil !</p>
         </div>
         </>
     )

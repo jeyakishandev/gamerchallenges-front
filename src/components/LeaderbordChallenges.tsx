d6233674ddd42115f13a30842dbd105084f620bf
MathiasLeleu <mathiasleleupro@gmail.com>
2025-05-14 14:20:01 +0200

    addition of emojis for the first 3 challenges in the ranking

--- src/components/LeaderbordChallenges.tsx
+++ src/components/LeaderbordChallenges.tsx
@@ -8,7 +8,14 @@ interface LeaderboardChallenges {
 
 // Rempli chaque ligne de la liste par les informations du challenge concerné
 export default function LeaderboardTopChallenges({ challenge, index}: LeaderboardChallenges) {
+
+    let medal = null;
+    if (index === 0) medal = "🥇";
+    else if (index === 1) medal ="🥈";
+    else if (index === 2) medal ="🥉";
+
     return (
-            <li>{index + 1} - <Link to={`/challenge/${challenge.id}`}> {challenge.name} : {challenge.users.length} participations </Link></li>
+            <li>{medal && <span style={{marginRight: "8px"}}>{medal}</span>}
+            {index + 1} - <Link to={`/challenge/${challenge.id}`}> {challenge.name} : {challenge.users.length} participations </Link></li>
     )
 }

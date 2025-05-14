df90ef3d7baeebdb3d0bf3343a31ece9a909e55a
MathiasLeleu <mathiasleleupro@gmail.com>
2025-05-14 17:05:27 +0200

    user avatar added to player rankings

--- src/components/LeaderboardPlayers.tsx
+++ src/components/LeaderboardPlayers.tsx
@@ -27,6 +27,11 @@ export default function LeaderboardTopPlayers ({players, index}: LeaderboardPlay
 
     return (
         <li className={className}>{medal && <span style={{marginRight: "8px"}}>{medal}</span>}
-            {index + 1}- <Link className={className}to={`/profile/${players.id}`}> {players.pseudo} : {players.challenges.length} challenges</Link> </li>
+            {index + 1}- <Link className={className}to={`/profile/${players.id}`}> <img
+                        className="avatar"
+                        src={`http://localhost:3000/uploads/${players?.avatar_url}`}
+                        alt={`Avatar de ${players?.pseudo || "l'utilisateur"}`}
+                        style={{ width: '20px', height: '20px', borderRadius: '50%' }}
+                    /> {players.pseudo} : {players.challenges.length} challenges</Link> </li>
     )
 }

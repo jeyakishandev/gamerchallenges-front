992603ef9768d316eb59cb81e6447017334d0963
MathiasLeleu <mathiasleleupro@gmail.com>
2025-05-14 14:18:49 +0200

    addition of emojis for the first 3 places in the ranking

--- src/components/LeaderboardPlayers.tsx
+++ src/components/LeaderboardPlayers.tsx
@@ -9,7 +9,14 @@ interface LeaderboardPlayers {
 
 // Rempli chaque ligne de la liste par les informations du joueur concerné
 export default function LeaderboardTopPlayers ({players, index}: LeaderboardPlayers) {
+
+    let medal = null;
+    if (index === 0) medal = "🥇";
+    else if (index === 1) medal ="🥈";
+    else if (index === 2) medal ="🥉";
+
     return (
-        <li>{index + 1}- <Link to={`/profile/${players.id}`}> {players.pseudo} : {players.challenges.length} challenges</Link> </li>
+        <li>{medal && <span style={{marginRight: "8px"}}>{medal}</span>}
+            {index + 1}- <Link to={`/profile/${players.id}`}> {players.pseudo} : {players.challenges.length} challenges</Link> </li>
     )
 }
